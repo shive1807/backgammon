fcd5b859aaf4e3f54d5497e4e4da9b398c3fd750
Shivam Bhati <shivambhati21@gmail.com>
2025-07-14 20:13:59 +0530

    Add UI system core classes and update scene
    Introduced UiRoot and UiSystem MonoBehaviour scripts for UI management. Added UiCanvasLayerDefinition to UiDefination.cs for canvas layer configuration. Updated Game.unity scene to remove dice managers and related prefab instances, reflecting changes in UI structure.

--- Backgammon/Assets/Scripts/Core/UiSystems/UiDefination.cs
+++ Backgammon/Assets/Scripts/Core/UiSystems/UiDefination.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Numerics;
 using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
 
 namespace Core.UiSystems
 {
@@ -43,6 +45,29 @@ namespace Core.UiSystems
         public override string ToString()
         {
             return $"{UiViewName} {AssetReference}";
+        }
+    }
+    
+    public class UiCanvasLayerDefinition
+    {
+        public readonly string Name;
+        public readonly int Ordinal;
+        public readonly Vector2 ReferenceResolution;
+        public readonly float MatchWidthOrHeight;
+        public readonly CanvasScaler.ScreenMatchMode ScreenMatchMode;
+
+        public UiCanvasLayerDefinition(string name, int ordinal, Vector2 referenceResolution, float matchWidthOrHeight, CanvasScaler.ScreenMatchMode screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight)
+        {
+            Name = name;
+            Ordinal = ordinal;
+            ReferenceResolution = referenceResolution;
+            MatchWidthOrHeight = matchWidthOrHeight;
+            ScreenMatchMode = screenMatchMode;
+
+            if (Ordinal < 0)
+            {
+                throw new ArgumentException("Ordinals smaller than zero are not allowed");
+            }
         }
     }
 }
---
+++ Backgammon/Assets/Scripts/Core/UiSystems/UiRoot.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Core.UiSystems
+{
+    public class UiRoot : MonoBehaviour
+    {
+        
+    }
+}
